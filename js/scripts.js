16a3152a2c8f129a24e93938f73e80fd5ddee824
percyelbis <colque.caillahua@gmail.com>
2022-12-21 21:57:18 -0500

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,595 @@
+// SIDEBAR TOGGLE
+
+var sidebarOpen = false;
+var sidebar = document.getElementById("sidebar");
+
+function openSidebar() {
+  if(!sidebarOpen) {
+    sidebar.classList.add("sidebar-responsive");
+    sidebarOpen = true;
+  }
+}
+
+function closeSidebar() {
+  if(sidebarOpen) {
+    sidebar.classList.remove("sidebar-responsive");
+    sidebarOpen = false;
+  }
+}
+
+
+
+// ---------- CHARTS ----------
+
+// BAR CHART
+var barChartOptions = {
+  series: [{
+    data: [238, 350, 618, 570, 1262],
+    name: "Lotes Catastrales",
+  }],
+  chart: {
+    type: "bar",
+    background: "transparent",
+    height: 350,
+    toolbar: {
+      show: false,
+    },
+  },
+  colors: [
+    "#2962ff",
+    "#d50000",
+    "#2e7d32",
+    "#ff6d00",
+    "#583cb3",
+  ],
+  plotOptions: {
+    bar: {
+      distributed: true,
+      borderRadius: 4,
+      horizontal: false,
+      columnWidth: "40%",
+    }
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  fill: {
+    opacity: 1,
+  },
+  grid: {
+    borderColor: "#55596e",
+    yaxis: {
+      lines: {
+        show: true,
+      },
+    },
+    xaxis: {
+      lines: {
+        show: true,
+      },
+    },
+  },
+  legend: {
+    labels: {
+      colors: "#f5f7ff",
+    },
+    show: true,
+    position: "top",
+  },
+  stroke: {
+    colors: ["transparent"],
+    show: true,
+    width: 2
+  },
+  tooltip: {
+    shared: true,
+    intersect: false,
+    theme: "dark",
+  },
+  xaxis: {
+    categories: ["Sector 04", "Sector 06", "Sector 07", "Sector 08", "Sector 11"],
+    title: {
+      style: {
+        color: "#f5f7ff",
+      },
+    },
+    axisBorder: {
+      show: true,
+      color: "#55596e",
+    },
+    axisTicks: {
+      show: true,
+      color: "#55596e",
+    },
+    labels: {
+      style: {
+        colors: "#f5f7ff",
+      },
+    },
+  },
+  yaxis: {
+    title: {
+      text: "Lotes",
+      style: {
+        color:  "#f5f7ff",
+      },
+    },
+    axisBorder: {
+      color: "#55596e",
+      show: true,
+    },
+    axisTicks: {
+      color: "#55596e",
+      show: true,
+    },
+    labels: {
+      style: {
+        colors: "#f5f7ff",
+      },
+    },
+  }
+};
+
+var barChart = new ApexCharts(document.querySelector("#bar-chart"), barChartOptions);
+barChart.render();
+
+
+
+
+// BAR CHART UNIDADES
+var barChartOptions = {
+  series: [{
+    data: [298, 740, 1836, 817, 1487],
+    name: "Unidades Catastrales",
+  }],
+  chart: {
+    type: "bar",
+    background: "transparent",
+    height: 350,
+    toolbar: {
+      show: false,
+    },
+  },
+  colors: [
+    "#2962ff",
+    "#d50000",
+    "#2e7d32",
+    "#ff6d00",
+    "#583cb3",
+    "#2962ff",
+    "#d50000",
+    "#2e7d32",
+    "#ff6d00",
+    "#583cb3",
+    "#2962ff",
+    "#d50000",
+    "#2e7d32",
+    "#ff6d00",
+    "#583cb3",
+    "#2962ff",
+    "#d50000",
+    "#2e7d32",
+    "#ff6d00",
+    "#583cb3",
+    "#2962ff",
+    "#d50000",
+    "#2e7d32",
+    "#ff6d00",
+    "#583cb3",
+    "#2962ff",
+    "#d50000",
+    "#2e7d32",
+    "#ff6d00",
+    "#583cb3",
+    "#2962ff",
+    "#d50000",
+    "#2e7d32",
+    "#ff6d00",
+    "#583cb3",
+    "#2962ff",
+    "#d50000",
+    "#2e7d32",
+    "#ff6d00",
+    "#583cb3",
+    "#2962ff",
+    "#d50000",
+    "#2e7d32",
+    "#ff6d00",
+    "#583cb3",
+    "#2962ff",
+    "#d50000",
+    "#2e7d32",
+    "#ff6d00",
+    "#583cb3",
+    "#2962ff",
+    "#d50000",
+    "#2e7d32",
+    "#ff6d00",
+    "#583cb3",
+    "#2962ff",
+    "#d50000",
+    "#2e7d32",
+    "#ff6d00",
+    "#583cb3",
+    "#2962ff",
+    "#d50000",
+    "#2e7d32",
+    "#ff6d00",
+    "#583cb3",
+    "#2962ff",
+    "#d50000",
+    "#2e7d32",
+    "#ff6d00",
+    "#583cb3",
+    "#2962ff",
+    "#d50000",
+    "#2e7d32",
+    "#ff6d00",
+    "#583cb3",
+    "#2962ff",
+    "#d50000",
+    "#2e7d32",
+  ],
+  plotOptions: {
+    bar: {
+      distributed: true,
+      borderRadius: 4,
+      horizontal: false,
+      columnWidth: "40%",
+    }
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  fill: {
+    opacity: 1,
+  },
+  grid: {
+    borderColor: "#55596e",
+    yaxis: {
+      lines: {
+        show: true,
+      },
+    },
+    xaxis: {
+      lines: {
+        show: true,
+      },
+    },
+  },
+  legend: {
+    labels: {
+      colors: "#f5f7ff",
+    },
+    show: true,
+    position: "top",
+  },
+  stroke: {
+    colors: ["transparent"],
+    show: true,
+    width: 2
+  },
+  tooltip: {
+    shared: true,
+    intersect: false,
+    theme: "dark",
+  },
+  xaxis: {
+    categories: ["Sector 04", "Sector 06", "Sector 07", "Sector 08", "Sector 11"],
+    title: {
+      style: {
+        color: "#f5f7ff",
+      },
+    },
+    axisBorder: {
+      show: true,
+      color: "#55596e",
+    },
+    axisTicks: {
+      show: true,
+      color: "#55596e",
+    },
+    labels: {
+      style: {
+        colors: "#f5f7ff",
+      },
+    },
+  },
+  yaxis: {
+    title: {
+      text: "Unidades Catastrales",
+      style: {
+        color:  "#f5f7ff",
+      },
+    },
+    axisBorder: {
+      color: "#55596e",
+      show: true,
+    },
+    axisTicks: {
+      color: "#55596e",
+      show: true,
+    },
+    labels: {
+      style: {
+        colors: "#f5f7ff",
+      },
+    },
+  }
+};
+
+var barChart = new ApexCharts(document.querySelector("#area-chart"), barChartOptions);
+barChart.render();
+
+
+
+
+
+
+
+// BAR componete urbano
+var barChartOptions = {
+  series: [{
+    data: [591,
+      22,
+      7,
+      41,
+      340,
+      422,
+      9,
+      121,
+      19,
+      1,
+      10,
+      96,
+      23,
+      9,
+      28,
+      2,
+      1,
+      5,
+      2,
+      3,
+      43,
+      1,
+      96,
+      3,
+      1,
+      9,
+      1,
+      28,
+      46,
+      3,
+      1,
+      1,
+      3,
+      509,
+      20,
+      374,
+      18,
+      57,
+      346,
+      1271,
+      1,
+      17,
+      96,
+      10,
+      4,
+      3,
+      3,
+      6,
+      4,
+      3,
+      70,
+      1072,
+      7,
+      10,
+      182,
+      223,
+      45,
+      45,
+      1,
+      6,
+      2,
+      2,
+      1,
+      2,
+      1,
+      2,
+      102,
+      3,
+      3,
+      2,
+      2,
+      1,
+      5,
+      1,
+      21,
+      1,
+      1,
+      9],
+    name: "Cantidad",
+  }],
+  chart: {
+    type: "bar",
+    background: "transparent",
+    height: 350,
+    toolbar: {
+      show: false,
+    },
+  },
+  colors: [
+    "#2962ff",
+    "#d50000",
+    "#2e7d32",
+    "#ff6d00",
+    "#583cb3",
+  ],
+  plotOptions: {
+    bar: {
+      distributed: true,
+      borderRadius: 4,
+      horizontal: false,
+      columnWidth: "40%",
+    }
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  fill: {
+    opacity: 1,
+  },
+  grid: {
+    borderColor: "#55596e",
+    yaxis: {
+      lines: {
+        show: true,
+      },
+    },
+    xaxis: {
+      lines: {
+        show: true,
+      },
+    },
+  },
+  legend: {
+    labels: {
+      colors: "#f5f7ff",
+    },
+    show: false,
+    position: "top",
+  },
+  stroke: {
+    colors: ["transparent"],
+    show: true,
+    width: 2
+  },
+  tooltip: {
+    shared: true,
+    intersect: false,
+    theme: "dark",
+  },
+  xaxis: {
+    
+    categories: ["CU_01",
+    "CU_02",
+    "CU_03",
+    "CU_04",
+    "CU_05",
+    "CU_06",
+    "CU_08",
+    "CU_10",
+    "CU_102",
+    "CU_103",
+    "CU_104",
+    "CU_105",
+    "CU_108",
+    "CU_111",
+    "CU_12",
+    "CU_123",
+    "CU_124",
+    "CU_13",
+    "CU_130",
+    "CU_132",
+    "CU_135",
+    "CU_137",
+    "CU_139",
+    "CU_14",
+    "CU_144",
+    "CU_15",
+    "CU_150",
+    "CU_153",
+    "CU_156",
+    "CU_157",
+    "CU_158",
+    "CU_159",
+    "CU_16",
+    "CU_17",
+    "CU_18",
+    "CU_19",
+    "CU_20",
+    "CU_21",
+    "CU_22",
+    "CU_24",
+    "CU_26",
+    "CU_27",
+    "CU_30",
+    "CU_32",
+    "CU_33",
+    "CU_34",
+    "CU_35",
+    "CU_37",
+    "CU_39",
+    "CU_40",
+    "CU_42",
+    "CU_43",
+    "CU_47",
+    "CU_49",
+    "CU_50",
+    "CU_51",
+    "CU_52",
+    "CU_54",
+    "CU_56",
+    "CU_59",
+    "CU_61",
+    "CU_66",
+    "CU_67",
+    "CU_68",
+    "CU_69",
+    "CU_76",
+    "CU_78",
+    "CU_79",
+    "CU_80",
+    "CU_83",
+    "CU_86",
+    "CU_89",
+    "CU_90",
+    "CU_91",
+    "CU_92",
+    "CU_93",
+    "CU_95",
+    "CU_97"],
+    title: {
+      style: {
+        color: "#f5f7ff",
+      },
+    },
+    axisBorder: {
+      show: true,
+      color: "#55596e",
+    },
+    axisTicks: {
+      show: true,
+      color: "#55596e",
+    },
+    labels: {
+      style: {
+        colors: "#f5f7ff",
+      },
+    },
+  },
+  yaxis: {
+    title: {
+      text: "Cantidad",
+      style: {
+        color:  "#f5f7ff",
+      },
+    },
+    axisBorder: {
+      color: "#55596e",
+      show: true,
+    },
+    axisTicks: {
+      color: "#55596e",
+      show: true,
+    },
+    labels: {
+      style: {
+        colors: "#f5f7ff",
+      },
+    },
+  }
+};
+
+var barChart = new ApexCharts(document.querySelector("#areacu-chart"), barChartOptions);
+barChart.render();
